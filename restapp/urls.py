9a63448b297f2fe907c69b7ebda9920590c5c4ab
Akash M <akashthor222@gmail.com>
2026-05-27 17:38:13 +0530

    first commit

---
+++ restapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from restapp import views
+from restapp.views import StudentAPI
+
+urlpatterns = [
+    # path('', views.index, name='index'),
+    path('students/', StudentAPI.as_view()),
+]
---
+++ restapp/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Student
+
+class StudentAPI(APIView):
+
+    def get(self,request):
+        all_students = Student.objects.all()
+
+        student_list = []
+        for s in all_students:
+            student_dict = {
+                "name" : s.name,
+                "age" : s.age,
+            }
+            student_list.append(student_dict)
+
+        return Response(student_list)
+
+    def post(self,request):
+        print(request.data)
+
+        new_student = Student(name = request.data['name'],age = request.data['age'])
+        new_student.save()
+
+        return Response("api")
